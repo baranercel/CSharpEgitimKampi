ed641205ab6c37e11a101b81aac61cb84d0f5911
baranercel01 <baranercell@gmail.com>
2026-03-27 23:23:31 +0300

    Döngü alıştırması yapıldı.

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++) {
+            //    Console.WriteLine("*");        
+            //    }
+
+            #endregion
+            #region Yanyana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++) {
+            //        Console.Write("* ");
+            //}
+
+            #endregion
+            #region Alta alta 10 tane yıldız oluşturma her satırda 10 tane yıldız = 10 * 10 kare
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            #endregion
+            #region Dik üçgen 
+
+            //for(int i = 1; i <= 5; i++)
+            //{ 
+            //   for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+            #region Ters Dik Üçgen
+
+            //for(int i = 5; i>=1; i--)
+            //{
+            //    for(int j = 1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+           
+            Console.Read();
+        }
+    }
+}
