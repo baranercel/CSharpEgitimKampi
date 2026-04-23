e7fef0cb513d4e29efdfcd9a68652ef774a1f227
baranercel01 <baranercell@gmail.com>
2026-04-23 23:17:27 +0300

    7.derse giriş yapıldı

---
+++ 07_ForEachLoop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ForEach Döngüsü
+
+            // ForEach(1;2;3;4)
+
+            //1.Değişken türü
+            //2:Değişken adı
+            //3:In
+            //4:Liste, Koleksiyon, Dizi
+
+            //string[] cities = { "Milano", "Budapeşte", "Lyon", "Kahire", "Üsküp" };
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 12, 13, 14, 15 };
+            //foreach (int number in numbers) { 
+
+            //    Console.WriteLine(number);
+            //
+
+            //int[]numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //foreach (int number in numbers) 
+            //{
+            //    if(number % 2 == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int total = 0;
+            //foreach (int i in numbers) 
+            //{
+            //    total += i;
+            //}
+            //Console.WriteLine(total);
+
+            //List<int> numbers = new List<int>() { 1,2,3,4,5,8};
+            //foreach (int i in numbers) {
+            //    Console.WriteLine(i);
+            //};
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
